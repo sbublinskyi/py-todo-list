ebc2ec85ad637074a874f5a0bf9896f5678576d9
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-14 11:47:47 +0200

    added create view for Task model, created task_form.html

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from todo.views import TaskListView, TagListView
+from todo.views import TaskListView, TagListView, TaskCreateView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
-    path("tags", TagListView.as_view(), name="tag-list")
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("tags/", TagListView.as_view(), name="tag-list")
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Task, Tag
@@ -5,6 +6,12 @@ from todo.models import Task, Tag
 
 class TaskListView(generic.ListView):
     model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
 
 
 class TagListView(generic.ListView):
