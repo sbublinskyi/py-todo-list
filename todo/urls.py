82858c7fcb49fc6678cf49bc0245ec17d3740ac1
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-14 11:56:29 +0200

    added updaet view for Task model, refactored task_form.html

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from todo.views import TaskListView, TagListView, TaskCreateView
+from todo.views import TaskListView, TagListView, TaskCreateView, TaskDeleteView, TaskUpdateView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagListView.as_view(), name="tag-list")
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -14,5 +14,16 @@ class TaskCreateView(generic.CreateView):
     success_url = reverse_lazy("todo:task-list")
 
 
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
 class TagListView(generic.ListView):
     model = Tag
