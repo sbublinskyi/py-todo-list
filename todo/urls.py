df8f15c032df045f6effbd5ce22436780ed29d43
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-14 11:24:25 +0200

    created task_list.htms, base.html & includes

--- todo/urls.py
+++ todo/urls.py
@@ -4,7 +4,7 @@ from todo.views import TaskListView, TagListView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
-    path("tags", TagListView.as_view(), name="tags-list")
+    path("tags", TagListView.as_view(), name="tag-list")
 ]
 
 app_name = "todo"
