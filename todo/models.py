51d9f41fe49b5784352aa5334a0730495ff329fe
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-14 12:59:46 +0200

    bugfixes of models, recreated migrations

--- todo/models.py
+++ todo/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=127)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Task(models.Model):
-    content = models.TextField()
+    content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateTimeField(blank=True)
+    deadline = models.DateTimeField(blank=True, null=True)
     is_completed = models.BooleanField()
     tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self):
+        return f"Task: {self.content}"
